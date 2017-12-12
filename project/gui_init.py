49ad14f35731a4283bd70ac18e9b6f0480b33c15
benjones4747 <benjones4747@gmail.com>
2017-12-12 16:00:33 +0000

    Add files via upload

--- project/gui_init.py
+++ project/gui_init.py
@@ -6,7 +6,13 @@ def welcome():
         root.destroy()
     def initialize():
         global mode
-        mode = "single player"
+        mode = "initialize poll.in"
+        label = Label(root, text= "polling coming soon!")
+        label.pack()
+
+    def exit():
+        global mode
+        mode = "exit poll.in"
         root.destroy()
 
     def two_player():
@@ -58,9 +64,10 @@ def welcome():
     frame.columnconfigure(0, weight=1)
     frame.rowconfigure(0, weight=1)
 
-    tk.Label(frame, text="Welcome to Polling Aggregator 0.1.1", bg = main_background_colour).grid(column = 1, row = 1,columnspan = 2)
+    tk.Label(frame, text="poll.in 0.1.1", bg = main_background_colour).grid(column = 1, row = 1,columnspan = 2)
     tk.Label(frame, text="Please select an option from below:", bg = main_background_colour).grid(column = 1, row = 2,columnspan = 2)
     tk.Button(frame, text="Initialize", command=initialize, bg = secondary_background_colour).grid(column=1, row=3,sticky= "we")
+    tk.Button(frame, text="Exit", command=exit, bg = secondary_background_colour).grid(column=1, row=4,sticky= "we")
     tk.Button(frame, text="See the Instructions", command=instructions, bg = secondary_background_colour).grid(column=1, row=4,sticky= "we")
     tk.Button(frame, text="See the Credits", command=show_credits, bg = secondary_background_colour).grid(column=2, row=4, sticky= "we")
     for child in frame.winfo_children(): child.grid_configure(padx=5, pady=5)#space everything out
